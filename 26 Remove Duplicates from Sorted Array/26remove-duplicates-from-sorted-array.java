fa98c528773e81ccd66e0ba1800162fa9dbb4788
kameshsuryavanshi <kameshsuryavanshi2712@gmail.com>
2024-03-04 15:51:09 +0000

    Code challenge submission for question: 26 Remove Duplicates from Sorted Array

---
+++ 26 Remove Duplicates from Sorted Array/26remove-duplicates-from-sorted-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+                if(nums[i] != nums[c]){
+                    c++;
+                   nums[c] = nums[i];                   
+                }
+            }
+            
+        return c+1;
+    }   
+    
+}
