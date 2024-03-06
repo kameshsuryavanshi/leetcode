0dbf3df8a055dc24f1a3c1e5d6e3cd0648651aa9
kameshsuryavanshi <kameshsuryavanshi2712@gmail.com>
2024-03-06 06:54:47 +0000

    Code challenge submission for question: 55 Jump Game

---
+++ 55 Jump Game/55jump-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false;
+            }
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (maxReach >= nums.length - 1) {
+                return true;
+            }
+        }
+        return true; // Changed to true since if we reach this point, it means we can reach the last index
+    }
+}
