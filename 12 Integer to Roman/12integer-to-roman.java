586cb021fe4d754d4117b671fbc5b3d41f7eb4bb
kameshsuryavanshi <kameshsuryavanshi2712@gmail.com>
2024-04-08 17:12:02 +0000

    Code challenge submission for question: 12 Integer to Roman

---
+++ 12 Integer to Roman/12integer-to-roman.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String intToRoman(int num) {
+            
+            int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+            String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+            StringBuilder s = new StringBuilder();
+            int index = 0;
+
+            while (num > 0) {
+                if (num - values[index] >= 0) {
+                    s.append(symbols[index]);
+                    num -= values[index];
+                } else {
+                    index++;
+                }
+            }
+
+            return s.toString();
+    
+    }
+}
