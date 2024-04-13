68cb406f9eab0257557c8f043675610490fd0ed3
kameshsuryavanshi <kameshsuryavanshi2712@gmail.com>
2024-04-13 13:13:22 +0000

    Code challenge submission for question: 125 Valid Palindrome

---
+++ 125 Valid Palindrome/125valid-palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        // Use two pointers approach without using StringBuilder
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            char leftChar = Character.toLowerCase(s.charAt(left));
+            char rightChar = Character.toLowerCase(s.charAt(right));
+
+            if (!Character.isLetterOrDigit(leftChar)) {
+                left++;
+            } else if (!Character.isLetterOrDigit(rightChar)) {
+                right--;
+            } else if (leftChar != rightChar) {
+                return false;
+            } else {
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
