ccf74b5e5cb79fd3040a78aedf9a42800f9df114
kameshsuryavanshi <kameshsuryavanshi2712@gmail.com>
2026-01-03 16:23:53 +0000

    Code challenge submission for question: 347 Top K Frequent Elements

---
+++ 347 Top K Frequent Elements/347top-k-frequent-elements.py
@@ -0,0 +1,17 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        buckets = [[] for _ in range(len(nums) + 1)]
+        
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+            
+        for n, c in count.items():
+            buckets[c].append(n)
+            
+        res = []
+        for i in range(len(buckets) - 1, 0, -1):
+            for n in buckets[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res      
